dac96bf4285823a9bdcb5e7e45ab60041b6f58da
ErickCesarA <erickcfalbuquerquef@gmail.com>
2023-05-15 13:05:56 -0300

    api-heroku-v0.3

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddEntityFrameworkSqlServer().AddDbContext<RegistroDePacientesDbContext>
@@ -25,16 +25,14 @@ builder.Services.AddCors(options => options.AddPolicy(name: "QuetionarioTccOrigi
     }));
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
+public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 
-app.UseSwagger();
-app.UseSwaggerUI();
+{ if (env.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    } }
 
 app.UseCors("QuetionarioTccOrigins");
 
