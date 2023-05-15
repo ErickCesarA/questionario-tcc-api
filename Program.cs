630aa98a658ef1a067b8cc0af2f2c96174a20a1b
ErickCesarA <erickcfalbuquerquef@gmail.com>
2023-05-15 12:36:57 -0300

    api-heroku-v0.2

--- Program.cs
+++ Program.cs
@@ -27,11 +27,14 @@ builder.Services.AddCors(options => options.AddPolicy(name: "QuetionarioTccOrigi
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("QuetionarioTccOrigins");
 
